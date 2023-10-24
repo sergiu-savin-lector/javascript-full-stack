f52198d8c0de617781ef64eca51373cf73a089c3
sergiu-savin-lector <s.savin@goit.global>
2023-10-24 21:32:57 +0300

    lectia 13

--- lectia-13/practica/script.js
+++ lectia-13/practica/script.js
@@ -1,3 +1,34 @@
+// Section Exercise 
+
+// const sectionEl = document.querySelector('.section-exercise');
+// const divEl = document.querySelector('.section-exercise div');
+// const paragraphEl = document.querySelector('.section-exercise p');
+
+// document.addEventListener('click', () => {
+//   console.log('Click pe document');
+// })
+
+// sectionEl.addEventListener('click', (e) => {
+//   e.stopPropagation();
+//   console.log('Ai apasat pe sectiune');
+// })
+
+// divEl.addEventListener('click', ()=> {
+//   console.log('Ai apasat pe div.')
+// })
+
+// paragraphEl.addEventListener('click', () => {
+//   console.log('Ai apasat pe paragraph');
+// })
+
+// Bubbling example
+
+// const countriesListEl = document.querySelector('.countries-list');
+
+// countriesListEl.addEventListener('click', (e) => {
+//   console.log('Ai dat click pe: ' + e.target.textContent)
+// })
+
 // Create a product cost calculator depending on the price. When working with events, use delegation. Render all markup via JS
 // When the data variable is entered in input #1 or #2, the total cost should be automatically recalculated
 // The currency - USD should always be displayed
@@ -7,25 +38,47 @@
 // In input No. 2, set min and max threshold by yourself.
 // When loading the page, the script should automatically calculate the cost based on the default data that you set in the markup.
 
+const formEl = document.querySelector('#form');
+const priceEl = document.querySelector('#price');
+const quantityEl = document.querySelector('#quantity');
+const amountDisplayEl = document.querySelector('#amount');
+const totalDisplayEl = document.querySelector('#total');
 
+const data = {
+  quantity: 0,
+  price: 0,
+  calcTotalPrice() {
+    return (this.quantity * this.price).toFixed(2)
+  }
+}
 
-// Section Exercise 
+data.quantity = quantityEl.getAttribute('value');
+data.price = priceEl.getAttribute('value');
+totalDisplayEl.textContent = data.calcTotalPrice() + ' USD';
 
-const sectionEl = document.querySelector('.section-exercise');
-const divEl = document.querySelector('.section-exercise div');
-const paragraphEl = document.querySelector('.section-exercise p');
+formEl.addEventListener('change', (e) => {
+  if (e.target === priceEl) {
+    console.log('Pret modificat ' + priceEl.value + ' pentru produsul si cantitatea ' + quantityEl.value);
 
-document.addEventListener('click', () => {
-  console.log('Click pe document');
+    console.log(`Pret modificat ${priceEl.value} pentru produsul si cantitatea ${quantityEl.value}`)
+
+    if(!Number(priceEl.value)) {
+      console.log('Not a number');
+      return;
+    }
+
+    data.price = priceEl.value;
+  }
+
+  if(e.target === quantityEl) {
+    console.log('Cantitate modificata');
+    data.quantity = quantityEl.value;
+  }
+
+  amountDisplayEl.textContent = quantityEl.value;
+  totalDisplayEl.textContent = data.calcTotalPrice() + ' USD';
 })
 
-sectionEl.addEventListener('click', (e) => {
-  debugger;
-  e.stopPropagation();
-  console.log('Ai apasat pe sectiune');
-})
 
-divEl.addEventListener('click', (e)=> {
-  console.log('Ai apasat pe div.')
-})
+
 
